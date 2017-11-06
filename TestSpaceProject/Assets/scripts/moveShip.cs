1125a2938e2127d1d0f6dd8b741bbbf29bd11073
JakartaIII <33393854+JakartaIII@users.noreply.github.com>
2017-11-06 15:24:06 +0500

    basic control

---
+++ TestSpaceProject/Assets/scripts/moveShip.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class moveShip : MonoBehaviour {
+	Rigidbody rigid;
+	public float x,y,xR,yR; 
+	public float speed, rotSpeed;
+	// Use this for initialization
+	void Start () {
+		rigid = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		InputKeys();
+		RigidbodyMove();
+	}
+	void RigidbodyMove()
+	{
+		rigid.AddForce(transform.forward*y*speed);
+		rigid.AddForce(transform.right*x*speed);
+		rigid.AddTorque(transform.up*xR*rotSpeed);
+		rigid.AddTorque(transform.right*-yR*rotSpeed);
+
+	}
+	void InputKeys()
+	{		
+		x =  Input.GetAxis("Horizontal");
+		y =  Input.GetAxis("Vertical");
+		xR =  Input.GetAxis("HorizontalRot");
+		yR =  Input.GetAxis("VerticalRot");
+	}
+}
---
+++ TestSpaceProject/Assets/scripts/portalScene.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class portalScene : MonoBehaviour {
+	public bool SecondSceen;
+	
+	void OnTriggerEnter(Collider other)
+	{
+		if(other.tag=="Player")
+		{
+			if(SecondSceen)
+			{
+				Application.LoadLevel(0);
+			} 
+			else
+			{
+				Application.LoadLevel(1);
+			}
+		}
+	}
+}
