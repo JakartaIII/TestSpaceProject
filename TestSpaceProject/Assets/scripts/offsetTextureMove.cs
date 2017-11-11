1f65f1807bfc2ab9c6449bcbd37c3ce62f62b79a
JakartaIII <33393854+JakartaIII@users.noreply.github.com>
2017-11-11 13:28:38 +0500

    Simple models added

---
+++ TestSpaceProject/Assets/scripts/offsetTextureMove.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class offsetTextureMove : MonoBehaviour {
+ [SerializeField] Material PortalTexture;
+ public float x;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		x+=Time.deltaTime*0.5f;
+		if(x>100) x =0; 
+		PortalTexture.mainTextureOffset = new Vector2(1,x);
+	}
+}
